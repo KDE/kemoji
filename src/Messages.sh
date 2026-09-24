6012bc5705a9735865b357f958e56f0a1e55ee10
Nicolas Fella <nicolas.fella@gmx.de>
2026-09-24 11:18:27 +0200

    Fix translations

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,9 +1,4 @@
-# SPDX-FileCopyrightText: 2026 James Graham <james.h.graham@protonmail.com>
-# SPDX-License-Identifier: LGPL-2.0-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
-
-#!/bin/sh
-
-# Extract strings from all source files.
-# EXTRACT_TR_STRINGS extracts strings with lupdate and convert them to .pot with
-# lconvert.
-$EXTRACT_TR_STRINGS `find . -name \*.cpp -o -name \*.h -o -name \*.ui -o -name \*.qml` -o $podir/kemoji_qt.pot
+#! /usr/bin/env bash
+# SPDX-FileCopyrightText: None
+# SPDX-License-Identifier: CC0-1.0
+$XGETTEXT `find . \( -name \*.cpp -o -name \*.h -o -name \*.qml \)` -o $podir/kemoji.pot
